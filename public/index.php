fdd11ea34d99ed1959d2c4dfe3123bf9aa1c4edc
juachalli <juachalli@alu.edu.gva.es>
2024-12-12 00:25:26 +0100

    First commit

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    echo "<div style='text-align:center;'>";
+    echo "<h3>Welcome to my web</h3>";
+    echo "<img src='images/under-construction.gif'/>";    
+    echo phpinfo();
+    echo "</div>";
+    //return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
